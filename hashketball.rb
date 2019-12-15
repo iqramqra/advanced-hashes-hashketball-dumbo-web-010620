4f5ae8cb90658bd367ab6fec520cc1a9c853d675
Iqra Masroor <masrooriqra@gmail.com>
2019-12-15 17:37:29 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -247,3 +247,13 @@ away_team = game_hash[:away][:team_name]
    return away_team
  end    
 end 
+
+def player_with_longest_name
+max_player = 0
+  game_hash.each do |team, team_hash|
+    team_hash[:players].each do |player, player_hash|
+     # p player_hash = home and away values, nested hashes 
+     p player_hash
+    end
+  end
+end
